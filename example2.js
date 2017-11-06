d7779db2cedda532e0567300295c0a7c0d586311
Rachata <paretk7@gmail.com>
2017-11-06 12:43:04 +0700

    first commit

---
+++ example2.js
@@ -0,0 +1,11 @@
+require('chromedriver');
+
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+  
